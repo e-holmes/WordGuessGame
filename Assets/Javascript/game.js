557928685c156fea303c7b0bd10adae7c2e82499
e-holmes <bethholmes768@gmail.com>
2019-09-12 23:32:46 -0700

    added button and java

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,4 +1,5 @@
 var mgWrds = ["wand", "cauldron", "magic", "wizard", "voldemort", "broomstick", "harry", "hermione", "ron", "peeves", "spells", "quidditch", "gryffindor", "hufflepuff", "slytherin", "ravenclaw", "werewolves", "transfiguration", "potions", "hippogriff" ];
+var help = ["Cast's spells.", "You put things in it.", "A life without it is a muggle life.", "A magical person", "The unnamed", "Needed for quidditch.", "A great hero.", "The founder of S.P.E.W.", "The master of light.", "A pesky ghost.", "They typically rhyme or are latin.", "A famous sport.", "House of the brave.", "The house of finders.", "No good comes from this house.", "The house of puzzles", "They are only dangerous once a month", "The best class for cat lovers.", "Drinking can have grave consequences.", "Harry, Hermione, and Sirius have all ridden one."]
 var wrd;
 var usrWrd;
 var ltrs = [];
@@ -6,20 +7,26 @@ var wins = 0;
 var losses = 0;
 var guess = 14;
 var user;
+var ran;
 
 //reset();
 reset();
 
 
-document.onkeyup = function(event){
+document.onkeypress = function(event){
     user = event.key;
     compare();  
 }
 
+function myfunction(){
+    document.getElementById("hint").innerHTML = "Here's a hint: " +help[ran];
+}
+
 function reset(){
+    ran=Math.floor(Math.random() *mgWrds.length);
     guess=14;
     document.getElementById("guesses").innerHTML = "Guesses Left: " +guess;
-    wrd = mgWrds[Math.floor(Math.random() *mgWrds.length)];
+    wrd = mgWrds[ran];
     console.log(wrd);
     usrWrd = wrd.split("");
     console.log(usrWrd);
@@ -29,6 +36,7 @@ function reset(){
     document.getElementById("letters").innerHTML = "Letters Used: " +ltrs;
     document.getElementById("wins").innerHTML = "Wins: " +wins;
     document.getElementById("losses").innerHTML = "Losses: " +losses;
+    document.getElementById("hint").innerHTML="";
 }
 
 
@@ -43,25 +51,18 @@ function updateD(){
     for (i=0; i <usrWrd.length; i++){
         if (user == wrd[i]){
             usrWrd[i] = wrd[i]
-            document.getElementById("userWord").innerHTML = usrWrd;
+            document.getElementById("userWord").innerHTML = usrWrd.join(" ");
         }
     }
 }
 
 function updateB(){
- //   var done = false;
- //   if (done){
-  //  for (y=0; y <usrWrd.length; y++){
-  //      if (user == ltrs[y]){
-   //         done = true;
-   //     }
-  //  }
-// }
+if (!ltrs.includes(user)){
     guess=guess-1;
     document.getElementById("guesses").innerHTML = "Guesses Left: " +guess;
     ltrs.splice(ltrs.length, 0, user);
     document.getElementById("letters").innerHTML = "Letters Used: " +ltrs;
-}
+}}
 
 function compare(){
     var ltrGsRt = false;
@@ -80,7 +81,7 @@ function compare(){
     if(ltrGsRt === false){
         updateB();
         if(guess <= 0){
-            losses--;
+            losses++;
             document.getElementById("losses").innerHTML = "losses" +losses;
             reset();
         }
